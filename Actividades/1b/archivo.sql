47f2b9795bcc9ad23a69bde8eb8a84aaddaf8609
NemiadLu <tomastorres1009008@gmail.com>
2026-05-18 20:31:40 -0300

    Delete Actividades directory

--- Actividades/1b/archivo.sql
+++
@@ -1,5 +0,0 @@
-SELECT descripcion, precio FROM Ropa;
-
-SELECT V.id_venta, V.tipo_factura, Ven.nombre AS Vendedor 
-FROM Venta V
-JOIN Vendedor Ven ON V.id_vendedor = Ven.id_vendedor;
--- Actividades/2b/archivo.sql
+++
@@ -1,6 +0,0 @@
-SELECT M.nombre, M.apellido, E.nombre_especialidad 
-FROM Medico M
-JOIN Medico_Especialidad ME ON M.id_medico = ME.id_medico
-JOIN Especialidad E ON ME.id_especialidad = E.id_especialidad;
-
-SELECT * FROM Turno;
