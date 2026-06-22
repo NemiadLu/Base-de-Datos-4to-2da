c4589c5c515502cbc3d19b2eae5537b2eb15110b
NemiadLuendo <tomastorres1009008@gmail.com>
2026-06-22 09:32:27 -0300

    Sql Realizado

--- Actividades/INSERTS.sql
+++ Actividades/INSERTS.sql
@@ -1,20 +1,19 @@
-/*insert into SERIES(id_serie, nombre_serie, anio_estreno) values
-(1, 'Hora de Aventura', 2010),
-(2, 'Ben 10', 2005),
-(3, 'Billy y Mandy', 2001),
-(4, 'Un Show Más', 2010),
-(5, 'Coraje el Perro Cobarde', 1999),
-(6, 'El laboratorio de Dexter', 1996);*/
+insert into SERIES(nombre_serie, anio_estreno) values
+('Hora de Aventura', 2010),
+('Ben 10', 2005),
+('Billy y Mandy', 2001),
+('Un Show Más', 2010),
+('Coraje el Perro Cobarde', 1999),
+('El laboratorio de Dexter', 1996);
 
-insert into PERSONAJES (id_personaje, nombre_personaje, rol, nivel_energia, su_serie) values
-(1, 'Finn el Humano', 'Protagonista', 85, 'Hora de Aventura'),
-(2, 'Jake el Perro ', 'Protagonista', 90, 'Hora de Aventura'),
-(3, 'Rey Helado', 'Antagonista', 75, 'Hora de Aventura'),
-(4, 'Ben Tennyson', 'Protagonista', 95, 'Ben 10'),
-(5, 'Vilgax', 'Antagonista', 98, 'Ben 10'),
-(6, 'Puro Hueso', 'Secundario', 80, 'Billy y Mandy'),
-(7, 'Mordecai', 'Protagonista', 60, 'Un Show Más'),
-(8, 'Rigby', 'Protagonista', 55, 'Un Show_Más'),
-(9, 'Coraje', 'Protagonista', 40, 'Coraje el Perro Cobarde'),
-(10, 'Dexter', 'Protagonista', 70, 'El laboratorio de Dexter');
-
+insert into PERSONAJES (nombre_personaje, rol, nivel_energia, id_serie) values
+('Finn el Humano', 'Protagonista', 85, 1),
+('Jake el Perro ', 'Protagonista', 90, 1),
+('Rey Helado', 'Antagonista', 75, 1),
+('Ben Tennyson', 'Protagonista', 95, 2),
+('Vilgax', 'Antagonista', 98, 2),
+('Puro Hueso', 'Secundario', 80, 3),
+('Mordecai', 'Protagonista', 60, 4),
+('Rigby', 'Protagonista', 55, 4),
+('Coraje', 'Protagonista', 40, 5),
+('Dexter', 'Protagonista', 70, 6);
--- Actividades/SELECT.sql
+++ Actividades/SELECT.sql
@@ -1,1 +1,9 @@
-select * from PERSONAJES
+SELECT 
+    S.id_serie,
+    S.nombre_serie,
+    S.anio_estreno,
+    P.nombre_personaje,
+    P.rol,
+    P.nivel_energia
+FROM SERIES S
+INNER JOIN PERSONAJES P ON S.id_serie = P.id_serie;
