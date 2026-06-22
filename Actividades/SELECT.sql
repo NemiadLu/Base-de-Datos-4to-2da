728ea97fe2156c6bb876d37d33f0074e98a361a9
NemiadLuendo <tomastorres1009008@gmail.com>
2026-06-22 09:35:50 -0300

    SQL Realizado #2

--- Actividades/SELECT.sql
+++ Actividades/SELECT.sql
@@ -1,9 +1,9 @@
-SELECT 
-    S.id_serie,
-    S.nombre_serie,
-    S.anio_estreno,
+SELECT
+    P.id_personaje,
     P.nombre_personaje,
+    S.nombre_serie,
     P.rol,
-    P.nivel_energia
+    P.nivel_energia,
+    S.id_serie
 FROM SERIES S
 INNER JOIN PERSONAJES P ON S.id_serie = P.id_serie;
